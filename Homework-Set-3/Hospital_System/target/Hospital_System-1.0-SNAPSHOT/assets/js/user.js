66675e58862c0ff0a0b8dfcac69d877d9286c2e4
kokol16 <kokol1616@gmail.com>
2021-11-27 20:35:11 +0200

    added user to see his data

--- Homework-Set-3/Hospital_System/target/Hospital_System-1.0-SNAPSHOT/assets/js/user.js
+++ Homework-Set-3/Hospital_System/target/Hospital_System-1.0-SNAPSHOT/assets/js/user.js
@@ -1,15 +1,24 @@
 
 function update_data() {
-
+    var gender
+    if ($('#man').is(':checked')) {
+        gender = "male"
+    } else {
+        gender = "female"
+    }
+    var blood_donor
+    if ($('#blood-giver').is(':checked')) {
+        blood_donor = 1
+    } else {
+        blood_donor = 0
+    }
     var data = {
-        username: $("#username").val(),
         email: $("#email").val(),
         password: $("#pswd").val(),
         firstname: $("#firstname").val(),
         lastname: $("#surname").val(),
         birthdate: $("#birth-date").val(),
         gender: gender,
-        amka: $("#amka").val(),
         country: $("#country").val(),
         city: $("#city").val(),
         address: $("#address").val(),
@@ -25,22 +34,35 @@ function update_data() {
 }
 
 function fill_user_info(responseData) {
-    username: $("#username").val(),
-    email: $("#email").val(),
-    password: $("#pswd").val(),
-    firstname: $("#firstname").val(),
-    lastname: $("#surname").val(),
-    birthdate: $("#birth-date").val(),
-    gender: gender,
-    amka: $("#amka").val(),
-    country: $("#country").val(),
-    city: $("#city").val(),
-    address: $("#address").val(),
-    telephone: $("#telephone").val(),
-    height: $("#height").val(),
-    weight: $("#weight").val(),
-    blooddonor: blood_donor,
-    bloodtype: $("#blood-type").val()
+    console.log(responseData)
+
+    $("#username").val(responseData.username)
+    $("#email").val(responseData.email)
+    $("#pswd").val(responseData.password)
+    $("#firstname").val(responseData.firstname)
+    $("#surname").val(responseData.lastname)
+    $("#birth-date").val(responseData.birthdate)
+    if (responseData.gender === "Male")
+        $("#man").prop("checked", true);
+    else
+        $("#woman").prop("checked", true);
+
+    if (responseData.blooddonor === "1")
+        $("#blood-giver").prop("checked", true);
+    else
+        $("#no").prop("checked", true);
+
+    $("#birth-date").val(responseData.birthdate)
+    $("#amka").val(responseData.amka)
+    $("#country").val(responseData.country)
+    $("#city").val(responseData.city)
+    $("#address").val(responseData.address)
+    $("#telephone").val(responseData.telephone)
+    $("#height").val(responseData.height)
+    $("#weight").val(responseData.weight)
+    var select_blood = '#blood-type option[value=\"' + responseData.bloodtype + '\"]'
+    console.log(select_blood)
+    $(select_blood).attr('selected', 'selected');
 }
 function get_data() {
     var xhr = new XMLHttpRequest();
